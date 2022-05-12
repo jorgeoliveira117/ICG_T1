39414e344730314e57d07cf96ab1b7a019b17a0e
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2022-05-12 01:15:42 +0100

    Added Ghost

--- scene.js
+++ scene.js
@@ -162,10 +162,164 @@ function load3DObjects(sceneGraph) {
     // ************************** //
     // Ghost
     // ************************** //
+    const ghost = new THREE.Group();
+    ghost.name = "ghost_1";
+    sceneGraph.add(ghost);
+    
+    ghost.position.set(0, 2, -3);
+    ghost.rotateY(Math.PI);
+
+    const ghostMaterial = new THREE.MeshPhongMaterial({ color: 0xFF0000 });
+    // Head
+    const ghostHeadGeometry = new THREE.SphereGeometry( 0.8, 32, 16, 0, 2*Math.PI, 0, Math.PI/2 );
+    const ghostHead = new THREE.Mesh( ghostHeadGeometry, ghostMaterial );
+    ghostHead.name = ghost.name + "_head";
+    ghost.add(ghostHead);
+    // Body
+    const ghostBodygeometry = new THREE.CylinderGeometry( 0.8, 0.8, 1, 32 );
+    const ghostBody = new THREE.Mesh( ghostBodygeometry, ghostMaterial );
+    ghostBody.castShadow = true;
+    ghostBody.position.set(0, -0.5, 0);
+    ghostBody.name = ghost.name + "_body";
+    ghost.add(ghostBody);
+
+    // Tail
+    const ghostTail = new THREE.Group();
+    ghostTail.name = ghost.name + "_tail";
+    ghostTail.SCALE_DOWN_SPEED = -0.05;
+    ghostTail.MIN_HEIGHT = -1;
+    ghostTail.MIN_SPEED = 0.1;
+    ghostTail.MAX_SPEED = 1;
+    ghostTail.position.set(0, -1, 0);
+
+    ghost.add(ghostTail);
+    const ghostTailSphere = new THREE.SphereGeometry(0.2, 8, 4);
+    const ghostTailSphereMaterial = new THREE.MeshToonMaterial({ color: 0xAA0000 });
+    var ghostTailBubble, rPos;
+    for(var i = 0; i < 15; i++){
+        ghostTailBubble = new THREE.Mesh(ghostTailSphere, ghostTailSphereMaterial);
+        ghostTail.speed = Math.random()*ghostTail.MAX_SPEED + ghostTail.MIN_SPEED;
+        ghostTail.add(ghostTailBubble);
+        rPos = getRandomPosition(0.6, 0.4);
+        ghostTailBubble.position.set(rPos.x, rPos.y, rPos.z);
+    }
+
+    // Normal Eyes
+    const ghostEyesGeometry = new THREE.SphereGeometry( 0.2, 16, 16, 0, 2*Math.PI, 0, Math.PI-Math.PI/6);
+    const ghostEyesMaterial = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
+    const ghostEyeRetinaGeometry = new THREE.SphereGeometry( 0.2, 16, 16, 0, 2*Math.PI, 0, Math.PI/6);
+    const ghostEyeRetinaMaterial = new THREE.MeshPhongMaterial( { color: 0x000000 } );
+
+    const ghostLeftEye = new THREE.Group();
+    ghostLeftEye.name = ghost.name + "_lefteye";
+
+    const ghostLeftEyeBall = new THREE.Mesh( ghostEyesGeometry, ghostEyesMaterial );
+    ghostLeftEyeBall.rotateX(Math.PI);
+    const ghostLeftRetina = new THREE.Mesh( ghostEyeRetinaGeometry, ghostEyeRetinaMaterial );
+    ghostLeftEye.add(ghostLeftRetina);
+    ghostLeftEye.add(ghostLeftEyeBall);
+    ghostLeftEye.position.set(0.4, 0.1, 0.6);
+    ghostLeftEye.rotateX(Math.PI/2);
+    ghost.add(ghostLeftEye);
+
+    const ghostRightEye = new THREE.Group();
+    ghostRightEye.name = ghost.name + "_righteye";
+    const ghostRightEyeBall = new THREE.Mesh( ghostEyesGeometry, ghostEyesMaterial );
+    ghostRightEyeBall.rotateX(Math.PI);
+    const ghostRightRetina = new THREE.Mesh( ghostEyeRetinaGeometry, ghostEyeRetinaMaterial );
+    ghostRightEye.add(ghostRightRetina);
+    ghostRightEye.add(ghostRightEyeBall);
+    ghostRightEye.position.set(-0.4, 0.1, 0.6);
+    ghostRightEye.rotateX(Math.PI/2);
+    ghost.add(ghostRightEye);
+
+
+
+    // Scared
+    const ghostScaredEyesGeometry = new THREE.SphereGeometry( 0.2, 16, 16);
+    const ghostScaredEyesMaterial = new THREE.MeshBasicMaterial( { color: 0xF5F0CB, side: THREE.DoubleSide } );
+    const ghostRightEyeScared = new THREE.Mesh( ghostScaredEyesGeometry, ghostScaredEyesMaterial );
+    ghostRightEyeScared.position.set(-0.4, 0.1, 0.6);
+    const ghostLeftEyeScared = new THREE.Mesh( ghostScaredEyesGeometry, ghostScaredEyesMaterial );
+    ghostLeftEyeScared.position.set(0.4, 0.1, 0.6);
+    
     
 
+    const ghostScaredMaterial = new THREE.MeshPhongMaterial({ color: 0x2129FF, emissive: 0x2129FF, specular: 0x2129FF, shininess: 20 });
+    
+    // Scared Mouth
+
+    const mouthShape = new THREE.Shape();
+    mouthShape.moveTo(-0.6, -0.5);
+    mouthShape.quadraticCurveTo(-0.4, -0.2 , -0.2, -0.5);
+    mouthShape.quadraticCurveTo(0.0, -0.2 , 0.2, -0.5);
+    mouthShape.quadraticCurveTo(0.4, -0.2 , 0.6, -0.5);
+    mouthShape.lineTo(0.6, -0.6);
+    mouthShape.quadraticCurveTo(0.4, -0.3 , 0.2, -0.6);
+    mouthShape.quadraticCurveTo(0.0, -0.3 , -0.2, -0.6);
+    mouthShape.quadraticCurveTo(-0.4, -0.3 , -0.6, -0.6);
+
+    const ghostMouthGeometry = new THREE.ShapeGeometry( mouthShape );
+    const ghostMouth = new THREE.Mesh( ghostMouthGeometry, ghostScaredEyesMaterial ) ;
+    ghostMouth.position.set(0, 0, 0.81);
+
+    const ghostScaredFace = new THREE.Group();
+    ghostScaredFace.name = ghost.name + "_scaredface";
+
+    ghostScaredFace.add(ghostRightEyeScared);
+    ghostScaredFace.add(ghostLeftEyeScared);
+    ghostScaredFace.add(ghostMouth);
+    ghost.add(ghostScaredFace);
+    
+    // Scared properties 
+    ghostBody.material = ghostScaredMaterial;
+    ghostHead.material = ghostScaredMaterial;
+    ghostTail.traverse((child) => {
+        child.material = ghostScaredMaterial;
+    })
+    ghostRightEye.visible = false;
+    ghostLeftEye.visible = false;
+    ghostScaredFace.visible = true;
+    
+
+    // Not scared properties
+    ghostBody.material = ghostMaterial;
+    ghostHead.material = ghostMaterial;
+    ghostTail.traverse((child) => {
+        child.material = ghostTailSphereMaterial;
+    })
+    ghostRightEye.visible = true;
+    ghostLeftEye.visible = true;
+    ghostScaredFace.visible = false;
 
 
+
+    
+    // Properties
+    ghost.BOB_SPEED = 0.25;
+    ghost.BOB_MAX_HEIGHT = 2.4;
+    ghost.BOB_MIN_HEIGHT = 1.8;
+
+}
+
+function getRandomPosition(radius, y){
+    // for x and z to be inside of the circle:
+    // x^2 + z^2 < radius
+
+    // assign x a value from 0 to radius
+    var x = Math.random() * radius;
+
+    // assign z a value from 0 to square root of (radius^2 - x^2)
+    var z = Math.random() * Math.sqrt(radius*radius - x*x);
+
+    // make x and z positive or negative
+
+    if (Math.floor(Math.random() * 2) - 1 < 0 )
+        x *= -1;
+    if (Math.floor(Math.random() * 2) - 1 < 0 )
+        z *= -1;
+
+    return new THREE.Vector3(x, Math.random() * y, z);
 }
 
 var mouseX = 0;
@@ -214,6 +368,10 @@ function computeFrame(time) {
     light.translateX(delta);
     */
 
+
+    // ************************** //
+    // Pacman
+    // ************************** //
     const pacmanModel = sceneElements.sceneGraph.getObjectByName("pacmanModel");
     
     // Mouth animation
@@ -265,6 +423,30 @@ function computeFrame(time) {
     }
 
     sceneElements.camera.lookAt(pacman.position);
+
+    // ************************** //
+    // Ghost
+    // ************************** //
+    const ghost = sceneElements.sceneGraph.getObjectByName("ghost_1");
+    const ghostTail = sceneElements.sceneGraph.getObjectByName("ghost_1_tail");
+    ghostTail.traverse( (child) => {
+        if(child.position.y <= ghostTail.MIN_HEIGHT){
+            child.position.y = 0;
+            child.scale.set(1, 1, 1);
+        }
+        child.translateY((Math.random()*ghostTail.MAX_SPEED + ghostTail.MIN_SPEED) * delta * -1);
+        child.scale.addScalar(ghostTail.SCALE_DOWN_SPEED*delta);
+    })
+    ghost.position.y += ghost.BOB_SPEED * delta;
+    if(ghost.position.y <= ghost.BOB_MIN_HEIGHT){
+        ghost.BOB_SPEED *= -1;
+        ghost.position.y = ghost.BOB_MIN_HEIGHT;
+    }
+    else if(ghost.position.y >= ghost.BOB_MAX_HEIGHT){
+        ghost.BOB_SPEED *= -1;
+        ghost.position.y = ghost.BOB_MAX_HEIGHT;
+    }
+
     // Rendering
     helper.render(sceneElements);
 
